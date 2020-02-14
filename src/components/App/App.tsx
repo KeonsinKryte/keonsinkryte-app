46934a3511ae3985687ee1a56c9339d1f61eecbe
KeonsinKryte <mauricioboterovallejo32@gmail.com>
2020-02-13 22:32:01 -0500

    Initial Commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Canvas from '../Canvas/Canvas';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Hola Mundo
+                <Canvas></Canvas>
+            </div>
+        );
+    }
+}
+
+export default App;
