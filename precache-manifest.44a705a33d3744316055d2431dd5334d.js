bf29db7389c7362ba2e4b45336bbcba583100cb7
KeonsinKryte <mauricioboterovallejo32@gmail.com>
2020-05-11 02:24:38 -0500

    Updates

---
+++ precache-manifest.44a705a33d3744316055d2431dd5334d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc07acb267c2e9152dfdea2a7e2e18e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f28bd073bf28afba24e0",
+    "url": "/static/css/main.2b185596.chunk.css"
+  },
+  {
+    "revision": "51c3efa75b2c37b0c9d7",
+    "url": "/static/js/2.a6c3d81a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6c3d81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f28bd073bf28afba24e0",
+    "url": "/static/js/main.8556a8b2.chunk.js"
+  },
+  {
+    "revision": "2a04464a83be3d03daf6",
+    "url": "/static/js/runtime-main.4d8febc3.js"
+  }
+]);
