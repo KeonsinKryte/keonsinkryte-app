9f5ee903518ec7e5b0c08c6484bf19fd05fe37d5
KeonsinKryte <mauricioboterovallejo32@gmail.com>
2020-05-11 01:03:58 -0500

    Updates

---
+++ precache-manifest.6d71f87f3497c64440382e5447b605c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d15908792dbe17ddb14bb35f3a906a",
+    "url": "/keonsinkryte-app/index.html"
+  },
+  {
+    "revision": "1c3972e5406dda422acf",
+    "url": "/keonsinkryte-app/static/css/main.17622dc0.chunk.css"
+  },
+  {
+    "revision": "15e61d81fcc217e1d9ba",
+    "url": "/keonsinkryte-app/static/js/2.b356af34.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/keonsinkryte-app/static/js/2.b356af34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c3972e5406dda422acf",
+    "url": "/keonsinkryte-app/static/js/main.32f3ae48.chunk.js"
+  },
+  {
+    "revision": "07b18f8ec91a9e0599be",
+    "url": "/keonsinkryte-app/static/js/runtime-main.deb735a1.js"
+  }
+]);
